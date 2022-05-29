53af204e7f15ec1800382a1d585f5029ced257f1
cristianjofre <megalocoman@gmail.com>
2022-05-29 19:47:48 -0400

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,39 @@ export class AppComponent {
   defaultQuestion = "pet";
   answer = "";
   genders = ["male", "female"];
+  user = {
+    username: "",
+    email: "",
+    secreQuestion: "",
+    answer: "",
+    gender: "",
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = "Superuser";
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: "",
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: "",
+    //   gender: "male",
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+      },
+    });
   }
 
-  onSubmit(form: NgForm) {
-    console.log(this.signupForm);
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secreQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
