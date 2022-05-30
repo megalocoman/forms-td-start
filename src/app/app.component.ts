69c0982a13a0177c482674f9ef0afe8aa9fb5495
cristianjofre <megalocoman@gmail.com>
2022-05-30 15:19:34 -0400

    forms angular done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,5 +45,7 @@ export class AppComponent {
     this.user.secreQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
